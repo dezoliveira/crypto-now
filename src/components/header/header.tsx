ea2fbefdfc7dc568dd1047d3151c905f4ef6f59c
Andres Oliveira <andresoliveira@protonmail.com>
2025-09-29 10:32:22 -0300

    feat: componente header com logo

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import styles from './header.module.css'
+import logoImg from '../../assets/logo.svg'
+import { Link } from 'react-router'
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <Link to="/">
+        <img src={logoImg} alt='Logo Cripto App' />
+      </Link>
+    </header>
+  )
+}
